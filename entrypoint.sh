0300347a190d17eacd0f3107256028ab6d66b894
schourasia-2-eightfold <135719195+schourasia-2-eightfold@users.noreply.github.com>
2024-02-27 16:35:58 +0530

    Update entrypoint.sh
    Add needs_ci:lite label on PR review

--- entrypoint.sh
+++ entrypoint.sh
@@ -40,12 +40,12 @@ number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
 labels=$(jq --raw-output .pull_request.labels[].name "$GITHUB_EVENT_PATH")
 sha=$(jq --raw-output .pull_request.head.sha "$GITHUB_EVENT_PATH")
 
-already_needs_ci=false
+already_needs_ci_lite=false
 
 for label in $labels; do
   case $label in
-    needs_ci)
-      already_needs_ci=true
+    needs_ci:lite)
+      already_needs_ci_lite=true
       ;;
     *)
       echo "Unknown label $label"
@@ -53,10 +53,10 @@ for label in $labels; do
   esac
 done
 
-if [[ "$already_needs_ci" == false ]]; then
+if [[ "$already_needs_ci_lite" == false ]]; then
   status=$(curl -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" -H "Content-Type: application/json" "${URI}/repos/${GITHUB_REPOSITORY}/statuses/${sha}" |  jq -r '.[] | select(.context=="Requisites") | .state' | head -1)
   if [[ $status != "success" ]]; then
-    echo "Adding label needs_ci"
-    add_label "needs_ci"
+    echo "Adding label needs_ci:lite"
+    add_label "needs_ci:lite"
   fi
 fi
